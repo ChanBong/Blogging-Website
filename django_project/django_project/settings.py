5c6aa9d9de1f9c361c9e758909a2d3c6cc89a0c1
ChanBong <harsh_k@ch.iitr.ac.in>
2021-02-08 20:31:37 +0530

    Allowed anybody to host

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@+u@($om_1y-st4e5a!$bs03z+$umzs!_!28968v4$z^cn@0q2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
